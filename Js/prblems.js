cc20b5085a376f62b6b1e4536dfa737339d276fa
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-05-18 14:50:26 +0600

    add some method

---
+++ Js/prblems.js
@@ -0,0 +1,28 @@
+/* Types :
+
+var num = 10;
+
+console.log(typeof(num)); */
+
+
+/* Convert string to num:
+
+var num = "10";
+num = parseInt(num);
+console.log(typeof(num)); */
+
+/*
+toFixed method:
+
+var num = 1.54623;
+console.log (num.toFixed(2)) */
+
+/* 
+toPrecision method: 
+
+var num = 1.54623;
+console.log (num.toPrecision(2)) */
+
+console.log(Number(10.5))
+console.log(Number(true))
+console.log(Number(false))
